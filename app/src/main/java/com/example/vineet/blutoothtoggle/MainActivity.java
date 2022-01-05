bc9f224970a2e53b4a5b2a907138272b7db67ede
Vineet Kumar <vineet.kumar_cs20@gla.ac.in>
2022-01-05 13:08:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vineet/blutoothtoggle/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.vineet.blutoothtoggle;
+
+import android.bluetooth.BluetoothAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+ToggleButton tb;
+    BluetoothAdapter ba;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tb=(ToggleButton)findViewById(R.id.toggleButton);
+        ba=BluetoothAdapter.getDefaultAdapter();
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                {
+                    ba.enable();
+                }
+                else
+                {
+                    ba.disable();
+                }
+            }
+        });
+
+    }
+}
